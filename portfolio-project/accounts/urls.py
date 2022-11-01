3c96fdda25b67561b9a0562f017c360f0f21ec35
Mintchoco12 <michaelzhang5654@gmail.com>
2022-11-01 19:33:08 +0100

    idek anymore

--- portfolio-project/accounts/urls.py
+++ portfolio-project/accounts/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('signup/', views.SignUp.as_view(), name='signup'),
     path('login/', views.Login.as_view(), name='login'),
+    path('logout/', views.logout_user, name='logout'),
+    path('profile/', views.profile, name='profile'),
 ]
--- portfolio-project/accounts/views.py
+++ portfolio-project/accounts/views.py
@@ -1,16 +1,25 @@
-from django.shortcuts import render
+from audioop import reverse
+from django.shortcuts import redirect, render
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import logout
 from django.urls import reverse_lazy
 from django.views import generic
 
 # Create your views here.
+
+class Login(generic.CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'registration/login.html'
 
 class SignUp(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'registration/signup.html'
 
-class Login(generic.CreateView):
-    form_class = UserCreationForm
-    success_url = reverse_lazy('login')
-    template_name = 'registration/login.html'
+def logout_user(request):
+    logout(request)
+    return redirect('home')
+
+def profile(request):
+    return render(request, 'registration/profile.html')
